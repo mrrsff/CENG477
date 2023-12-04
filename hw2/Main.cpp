23e9ea14644a55a08779a9f51d021d1069f6ab56
Jabloon <alperen.tasdelen@metu.edu.tr>
2023-12-04 22:03:46 +0300

    notes added to Main.cpp

--- hw2/Main.cpp
+++ hw2/Main.cpp
@@ -4,6 +4,28 @@
 
 using namespace std;
 
+/*
+    1. Transform vertices to the viewport
+    2. Line drawing algorithm
+    3. Triangle rasterization
+    4. Clipping Algorithm
+        4.1 Cohen-Sutherland (simple)
+        4.2 Liang-Barsky (better)
+    5. Backface Culling
+
+    Notes:
+    - The rasterizer should be able to handle multiple objects
+    - No lighting, just interpolation of colors
+    - Projection transformations: Orthographic and Perspective
+    - Clipping: Cohen-Sutherland and Liang-Barsky
+    - Backface Culling: Dot product of the normal and the view vector
+    - Rasterization: Barycentric coordinates
+    - Line drawing: Midpoint Algorithm
+    - Wireframe and Solid rendering
+    - Depth Buffer Algorithm
+    - A makefile should be provided (make rasterizer)
+*/
+
 Scene *scene;
 
 int main(int argc, char *argv[])
